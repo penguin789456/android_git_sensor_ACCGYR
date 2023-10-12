0abfdd53c6010160cbdadcfc49b01c2d4c966cba
11010112 <11010112@gm.chihlee.edu.tw>
2023-10-12 14:34:15 +0800

    set GYROSCOPE values to showText

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.myapplication
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),SensorEventListener {
     private lateinit var gx:TextView
     private lateinit var gy:TextView
     private lateinit var gz:TextView
@@ -27,5 +29,15 @@ class MainActivity : AppCompatActivity() {
         gx=findViewById(R.id.showX)
         gy=findViewById(R.id.showY)
         gz=findViewById(R.id.showZ)
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        gx.text= p0!!.values[0].toString()
+        gy.text= p0!!.values[1].toString()
+        gz.text= p0!!.values[2].toString()
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
     }
 }

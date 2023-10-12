b0a9481b0c05fe97d72b7add011745b349f6cab8
11010112 <11010112@gm.chihlee.edu.tw>
2023-10-12 15:56:05 +0800

    features ACCELEROMETER fin

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,9 +13,13 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     private lateinit var gx:TextView
     private lateinit var gy:TextView
     private lateinit var gz:TextView
+    private lateinit var ax:TextView
+    private lateinit var ay:TextView
+    private lateinit var az:TextView
 
     private  lateinit var sensorManager:SensorManager
     private lateinit var grySensor:Sensor
+    private lateinit var accSensor:Sensor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,17 +27,22 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         init()
 
         sensorManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        grySensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        grySensor=sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        accSensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     }
     private fun init(){
         gx=findViewById(R.id.showX)
         gy=findViewById(R.id.showY)
         gz=findViewById(R.id.showZ)
+        ax=findViewById(R.id.ACCELEROMETERx)
+        ay=findViewById(R.id.ACCELEROMETERy)
+        az=findViewById(R.id.ACCELEROMETERz)
     }
 
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(this,grySensor,SensorManager.SENSOR_DELAY_UI)
+        sensorManager.registerListener(this,grySensor,SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this,accSensor,SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
@@ -46,6 +55,11 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
             gx.text= p0!!.values[0].toString()
             gy.text= p0!!.values[1].toString()
             gz.text= p0!!.values[2].toString()
+        }
+        if(p0?.sensor?.type==Sensor.TYPE_ACCELEROMETER){
+            ax.text= p0!!.values[0].toString()
+            ay.text= p0!!.values[1].toString()
+            az.text= p0!!.values[2].toString()
         }
     }
 

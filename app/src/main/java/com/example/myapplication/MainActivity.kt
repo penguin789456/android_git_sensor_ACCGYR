9e3eb1aeea390aa56af5cfd8380fcb0a3de0a89b
11010112 <11010112@gm.chihlee.edu.tw>
2023-10-12 14:08:03 +0800

    init GYROSCOPE and xml object

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var gx:TextView
+    private lateinit var gy:TextView
+    private lateinit var gz:TextView
+
+    private  lateinit var sensorManager:SensorManager
+    private lateinit var grySensor:Sensor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+
+        sensorManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        grySensor=sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+    }
+    private fun init(){
+        gx=findViewById(R.id.showX)
+        gy=findViewById(R.id.showY)
+        gz=findViewById(R.id.showZ)
     }
 }
